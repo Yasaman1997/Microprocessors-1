7d3c455788a58e5cf205902bdd6e8af5d81025c4
Ali Gholami <aligholami7596@gmail.com>
2017-03-17 00:20:11 +0330

    write the data and set EEMWER bit in EECR

--- ATmega Programming/EEPROM WRITING PRACTICE/EEPROM_WRITE.asm
+++ ATmega Programming/EEPROM WRITING PRACTICE/EEPROM_WRITE.asm
@@ -23,12 +23,16 @@ EEPROM_WRITE:
 	rjmp EEPROM_WRITE
 	
 	/* Write the address to be filled with the number :D */
-		
+	out EEARH,R18
+	out EEARH,R19	
 	
-	
-	
-	
-	
+	/* Write the data */
+	/* Counter can be used itself */
+	out EEDR,R16
+
+	/* Write logical one to the EEMWE */
+	/* Set bit immediate */
+	sbi EECR,EEMWE
 	/* Add 1 to the counter */
 	add R16,1
 	/* Check the loop end point */
