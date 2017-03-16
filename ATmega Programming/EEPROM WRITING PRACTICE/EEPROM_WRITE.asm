99c48336f8d16dec45670a24a09d03328b1dbe98
Ali Gholami <aligholami7596@gmail.com>
2017-03-17 00:05:03 +0330

    adding a loop for EECR and End Point

--- ATmega Programming/EEPROM WRITING PRACTICE/EEPROM_WRITE.asm
+++ ATmega Programming/EEPROM WRITING PRACTICE/EEPROM_WRITE.asm
@@ -8,5 +8,21 @@
 
 ; Replace with your application code
 start:
-    
-    rjmp start
+	/* Define a counter in R16 */
+	ldi R16,0
+	ldi R17,8
+	/* Loop through this untill 9 numbers are written */
+EEPROM_WRITE:
+    /* Wait untill the EEWE gets 0 */
+	/* Skip next instruction if EEWE is clear in EECR */
+	sbic EECR,EEWE
+	rjmp EEPROM_WRITE
+	/* Add 1 to the counter */
+	add R16,1
+	/* Check the loop end point */
+	eor R16,R17
+
+    rjmp EEPROM_WRITE
+	
+	rjmp start
+
