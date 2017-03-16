95fb7f3669f4e60b5bc168a5810503111ace3f98
Ali Gholami <aligholami7596@gmail.com>
2017-03-17 00:17:30 +0330

    Loop completed | address is in R18 and R19

--- ATmega Programming/EEPROM WRITING PRACTICE/EEPROM_WRITE.asm
+++ ATmega Programming/EEPROM WRITING PRACTICE/EEPROM_WRITE.asm
@@ -11,18 +11,29 @@ start:
 	/* Define a counter in R16 */
 	ldi R16,0
 	ldi R17,8
+	/* EEPROM Address to be written */
+	ldi R18,0x00
+	ldi R19,0x00
+
 	/* Loop through this untill 9 numbers are written */
 EEPROM_WRITE:
     /* Wait untill the EEWE gets 0 */
 	/* Skip next instruction if EEWE is clear in EECR */
 	sbic EECR,EEWE
 	rjmp EEPROM_WRITE
+	
+	/* Write the address to be filled with the number :D */
+		
+	
+	
+	
+	
+	
 	/* Add 1 to the counter */
 	add R16,1
 	/* Check the loop end point */
-	eor R16,R17
-
+	cp R16,R17
+	brne EEPROM_WRITE
     rjmp EEPROM_WRITE
-	
 	rjmp start
 
