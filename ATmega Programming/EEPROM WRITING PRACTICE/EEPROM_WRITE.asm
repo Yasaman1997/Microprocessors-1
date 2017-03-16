6b06cfffad55b715e33b5e583fdb852fbead479f
Ali Gholami <aligholami7596@gmail.com>
2017-03-17 00:26:36 +0330

    Writing Completed

--- ATmega Programming/EEPROM WRITING PRACTICE/EEPROM_WRITE.asm
+++ ATmega Programming/EEPROM WRITING PRACTICE/EEPROM_WRITE.asm
@@ -14,34 +14,27 @@ start:
 	/* EEPROM Address to be written */
 	ldi R18,0x00
 	ldi R19,0x00
-
 	/* Loop through this untill 9 numbers are written */
 EEPROM_WRITE:
     /* Wait untill the EEWE gets 0 */
 	/* Skip next instruction if EEWE is clear in EECR */
 	sbic EECR,EEWE
-	rjmp EEPROM_WRITE
-	
+	rjmp EEPROM_WRITE	
 	/* Write the address to be filled with the number :D */
 	out EEARL,R18
 	out EEARH,R19	
-	
 	/* Write the data */
 	/* Counter can be used itself */
 	out EEDR,R16
-
 	/* Write logical one to the EEMWE */
 	/* Set bit immediate */
 	sbi EECR,EEMWE
-
 	/* Start write */
 	sbi EECR,EEWE
-
 	/* Add 1 to the counter */
 	add R16,1
 	/* Go to the next address on EEPROM */
 	add R18,1
-
 	/* Check the loop end point */
 	cp R16,R17
 	brne EEPROM_WRITE
