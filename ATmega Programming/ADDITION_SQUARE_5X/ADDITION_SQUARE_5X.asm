9b9513e93f33f4516c59a689055573e71d7f08c5
Ali Gholami <aligholami7596@gmail.com>
2017-03-17 22:22:13 +0330

    Change the R7 to R20 (limitations)

--- ATmega Programming/ADDITION_SQUARE_5X/ADDITION_SQUARE_5X.asm
+++ ATmega Programming/ADDITION_SQUARE_5X/ADDITION_SQUARE_5X.asm
@@ -13,27 +13,26 @@ adder_subroutine:
 	ldi R10,25
 	/* Find the square of the number */ 
 	/* R7 will start this procedure from 0 and will be added 5 more each time */
-	ldi R7,0
-	/* Sqaure the R7 and put the result in R1:R0(mul does this by default) */
-	mul R7,R7
+	ldi R20,0
+	/* Sqaure the R20 and put the result in R1:R0(mul does this by default) */
+	mul R20,R20
 	/* add the result to the R4:R3 (we need addition! not replacement :D) */
 	add R3,R0
 	adc R4,R1			
-	/* We need to use R6 with R7 as a pair(since ADIW will put the result in a pair) */
-	/*ldi R6,0 (shame) */
-	/* Add immediate 5 to the word R7:R6 (because we need the sqaure of 5X numbers) */
-	/*adiw R7:R6,5 (shame) */
-	/* i guess we need to increment R7 5 times :| */ 
-	inc R7
-	inc R7
-	inc R7
-	inc R7 
-	inc R7
+	/* We need to use R19 with R20 as a pair(since ADIW will put the result in a pair) */
+	/*ldi R19,0 (shame) */
+	/* Add immediate 5 to the word R20:R19 (because we need the sqaure of 5X numbers) */
+	/*adiw R20:R19,5 (shame) */
+	/* i guess we need to increment R20 5 times :| */ 
+	inc R20
+	inc R20
+	inc R20
+	inc R20 
+	inc R20
 	/* Check if we have reached the end of the loop */
-	cp R7,R10
+	cp R20,R10
 	brne adder_subroutine
 	/* Final addition result is stored in R4:R3 */ 
 	/* Put the result back to the R1:R0 */
 	movw R1:R0,R4:R3
-
   rjmp start
