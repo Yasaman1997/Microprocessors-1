0b36b85f4452c54f8e4ea71c58e68382ac7d5d54
Ali Gholami <aligholami7596@gmail.com>
2017-03-17 21:09:54 +0330

    R9 is going to be added 5 more each time we loop

--- ATmega Programming/ADDITION_SQUARE_5X/ADDITION_SQUARE_5X.asm
+++ ATmega Programming/ADDITION_SQUARE_5X/ADDITION_SQUARE_5X.asm
@@ -10,4 +10,7 @@
 start:
 	/* The number n must be stored in R10 according to the problem definition */ 
 	ldi R10,25
+	/* Make a copy of R10 in the R9 */ 
+	/* R9 is going to be added 5 numbers in each loop */
+	mov R9,R10
     rjmp start
