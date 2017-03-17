f3c3c8a793265bd17abbbb7f74d7581e5a304086
Ali Gholami <aligholami7596@gmail.com>
2017-03-17 21:18:07 +0330

    .def makes me feel like im coding c++ :dancer:

--- ATmega Programming/ADDITION_SQUARE_5X/ADDITION_SQUARE_5X.asm
+++ ATmega Programming/ADDITION_SQUARE_5X/ADDITION_SQUARE_5X.asm
@@ -9,8 +9,9 @@
 ; Replace with your application code
 start:
 adder_subroutine:
+	.def n,25
 	/* The number n must be stored in R10 according to the problem definition */ 
-	ldi R10,25
+	ldi R10,n
 	/* Make a copy of R10 in the R9 */ 
 	/* R9 is to check the end of the loop */
 	mov R9,R10
