46decff827402f207eb7cc3067ec034d1ad97b1c
Ali Gholami <aligholami7596@gmail.com>
2017-03-17 21:13:05 +0330

    R9 is to check the end of the loop | end checker :P

--- ATmega Programming/ADDITION_SQUARE_5X/ADDITION_SQUARE_5X.asm
+++ ATmega Programming/ADDITION_SQUARE_5X/ADDITION_SQUARE_5X.asm
@@ -8,9 +8,15 @@
 
 ; Replace with your application code
 start:
+adder_subroutine:
 	/* The number n must be stored in R10 according to the problem definition */ 
 	ldi R10,25
 	/* Make a copy of R10 in the R9 */ 
-	/* R9 is going to be added 5 numbers in each loop */
+	/* R9 is to check the end of the loop */
 	mov R9,R10
-    rjmp start
+
+	/* Check if we have reached the n */
+	cp R9,R10
+	brne adder_subroutine
+
+  rjmp start
