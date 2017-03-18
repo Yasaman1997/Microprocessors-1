a0713c6a321038a36477becfa7788c4ddce33aca
Ali Gholami <aligholami7596@gmail.com>
2017-03-18 19:05:36 +0330

    some comments for clarification

---
+++ ATmega Programming/ARRAY SORTING/ARRAY_SORT_ASSEMBLY.asm
@@ -0,0 +1,14 @@
+;
+; ARRAY_SORT_ASSEMBLY.asm
+;
+; Created: 3/18/2017 7:02:47 PM
+; Author : Ali Gholami
+;
+
+
+; Replace with your application code
+start:
+	/* Using bubble sort to sort the array */
+	/* The data is pre-written into the ARRAY label in the program memory */ 
+	/* So we can access the ARRAY address by writing array in any instruction */
+    rjmp start
