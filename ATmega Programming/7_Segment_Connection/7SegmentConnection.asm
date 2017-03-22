a7b6d9b3099bb558c8b40b5efec07c63df1da05a
Ali Gholami <aligholami7596@gmail.com>
2017-03-22 21:10:40 +0430

    do this on proteus (not installed yet :D)

---
+++ ATmega Programming/7_Segment_Connection/7SegmentConnection.asm
@@ -0,0 +1,14 @@
+;
+; 7SegmentConnection.asm
+;
+; Created: 3/22/2017 8:59:55 PM
+; Author : Ali Gholami
+;
+
+
+; Replace with your application code
+start:
+	/* Connect the PORT B to the 7-Segment */
+	/* This part must be done in the proteus simulator */
+
+    rjmp start
