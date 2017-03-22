11da4f7a746bebcce0302497fae308cf0ada3e23
Ali Gholami <aligholami7596@gmail.com>
2017-03-22 23:09:04 +0430

    add configuration section

--- ATmega Programming/R_W_DISPLAY/R_W_DISPLAY.asm
+++ ATmega Programming/R_W_DISPLAY/R_W_DISPLAY.asm
@@ -44,5 +44,25 @@ EEPROM_WRITE:
 	/**************************/
 
 
-	/*
+	/**************************/
+	/* Configure port B and 7 SEG */
+		/* Connect the PORT B to the 7-Segment */
+	/* This part must be done in the proteus simulator */
+
+	/* Put the direction of port B to output */
+	/* to do this we use DDR(Data Direction Register) and turn all ports to 1 */
+	ldi R16,0xFF	; Load 0b11111111 in R16
+    out DDRB,R16	; Configure the PORT B as output 
+
+	/* To reserve some place for the 7Segment encoding table in the memory
+	* we need to use .db directive :)
+	* The DB directive reserves memory resources in the program memory or the EEPROM memory.
+	* In order to be able to refer to the reserved locations, the DB directive should be preceded by a label. 
+	* The DB directive takes a list of expressions,
+	* and must contain at least one expression. The DB directive must be placed in a Code Segment or an EEPROM Segment. */
+	.CSEG
+	BCDTo7-Seg: .DB 0xCF, 0xA4, 0xB0, 0x00, 0x99, 0x92, 0x82, 0xF8, 0x80
+	/**************************/
+
+
     rjmp start
