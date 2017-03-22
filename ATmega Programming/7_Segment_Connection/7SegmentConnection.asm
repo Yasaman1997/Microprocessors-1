c71d65eb76848a3ebcb4abddca03dbda119f4c41
Ali Gholami <aligholami7596@gmail.com>
2017-03-22 21:38:22 +0430

    .db directive definition and clarification

--- ATmega Programming/7_Segment_Connection/7SegmentConnection.asm
+++ ATmega Programming/7_Segment_Connection/7SegmentConnection.asm
@@ -13,7 +13,15 @@ start:
 
 	/* Put the direction of port B to output */
 	/* to do this we use DDR(Data Direction Register) and turn all ports to 1 */
+	ldi R16,0xFF	; Load 0b11111111 in R16
+    out DDRB,R16	; Configure the PORT B as output 
+
+	/* To reserve some place for the 7Segment encoding table in the memory
+	* we need to use .db directive :)
+	* The DB directive reserves memory resources in the program memory or the EEPROM memory.
+	* In order to be able to refer to the reserved locations, the DB directive should be preceded by a label. 
+	* The DB directive takes a list of expressions,
+	* and must contain at least one expression. The DB directive must be placed in a Code Segment or an EEPROM Segment. */
 	
-	/* Put the numbers which are going to be written on DDRB in another register(like R16) */
-	ldi R16,(1<<PB0)
-    rjmp start
+   
+    rjmp start		
