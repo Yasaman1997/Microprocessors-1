a7710caf63b3fe823566803aadc02967c2cf3563
Ali Gholami <aligholami7596@gmail.com>
2017-03-22 21:29:34 +0430

    Configure the PORT B as output

--- ATmega Programming/7_Segment_Connection/7SegmentConnection.asm
+++ ATmega Programming/7_Segment_Connection/7SegmentConnection.asm
@@ -11,4 +11,9 @@ start:
 	/* Connect the PORT B to the 7-Segment */
 	/* This part must be done in the proteus simulator */
 
+	/* Put the direction of port B to output */
+	/* to do this we use DDR(Data Direction Register) and turn all ports to 1 */
+	
+	/* Put the numbers which are going to be written on DDRB in another register(like R16) */
+	ldi R16,(1<<PB0)
     rjmp start
