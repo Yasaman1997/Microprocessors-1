11951164d6b6858f2bfecdfad56dece05203ae0d
Ali Gholami <aghce@aut.ac.ir>
2017-04-01 17:54:44 +0330

    A program to turn the led on/off manually

---
+++ ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
@@ -0,0 +1,12 @@
+;
+; LED_TEST.asm
+;
+; Created: 4/1/2017 5:33:03 PM
+; Author : Ali Gholami
+;
+; Program to test the LED ON/OFF states manually
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
