e95c780a77fde8dc2131c34aa158fabd0bed2238
Ali Gholami <aghce@aut.ac.ir>
2017-04-01 18:58:22 +0330

    Source was added from preious LED test

--- ATmega Programming/LED_TEST/LED_10_TIME_BLINKER/LED_10_TIME_BLINKER.asm
+++ ATmega Programming/LED_TEST/LED_10_TIME_BLINKER/LED_10_TIME_BLINKER.asm
@@ -4,9 +4,34 @@
 ; Created: 4/1/2017 6:53:17 PM
 ; Author : Ali Gholami
 ;
-
+; Program to blink the LED 10 times with delay each time we press the button 
 
 ; Replace with your application code
 start:
-    
-    rjmp start
+    /* set the PINB0 data direction to 0 for input */
+	/* This one simulates the key */
+	ldi R16, 0x01	; Load 0b00000001 in R16
+    out DDRB,R16	; Configure the PINB0 as input
+
+	/* set the PORTB data direction to 1 for output */
+	/* this one causes the LED to be ON/OFF */
+	ldi R17, 0x80	; Load 0b10000000 in R17
+	out DDRB,R17	; Configure the PORTB7 as output
+
+OFF_MODE:
+	/* Put the PORTB to 0 */ 
+	ldi R18,0x00
+	out PORTB,R18
+	/* Check the content of PINB0 */
+	ldi R19,0x01	; Load 0b00000001 in R19
+	cp PINB,R19		; Compare the contents to see if the button is pressed
+	brne OFF_MODE	; Branch to the OFF_MODE if the key isn't pressed yet
+ON_MODE:
+	/* Put the PORTB to 1 */
+	ldi R18,0xFF
+	out PORTB,R18
+	/* Check the content of PINB0 */
+	ldi R19,0x01	; Load 0b00000001 in R19
+	cp PINB,R19		; Compare the contents to see if the button is unpressed */
+	brne ON_MODE	; Branch to the ON_MODE if the key isn't unpressed yet
+rjmp start
