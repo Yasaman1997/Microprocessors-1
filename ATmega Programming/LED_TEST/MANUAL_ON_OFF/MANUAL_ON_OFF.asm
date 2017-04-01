bc7ee7fcdefeef43f01a889a9097476d0c3ab8bf
Ali Gholami <aghce@aut.ac.ir>
2017-04-01 18:20:24 +0330

    Branch to the OFF_MODE if the key isn't pressed yet

--- ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
+++ ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
@@ -11,4 +11,10 @@ start:
     /* set the port b data direction to 0 for input */
 	ldi R16, 0x0F	; Load 0b00000001 in R16
     out DDRB,R16	; Configure the PORTB0 as output
+OFF_MODE:
+	/* Check the content of PORTB0 */
+	ldi R17,0x01	; Load 0b00000001 in R16
+	cp PINB,R17		; Compare the contents to see if the button is pressed
+	brne OFF_MODE	; Branch to the OFF_MODE if the key isn't pressed yet
+
 	rjmp start
