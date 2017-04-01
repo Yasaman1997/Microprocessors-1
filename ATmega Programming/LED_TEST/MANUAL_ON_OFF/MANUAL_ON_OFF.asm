1356d743d2fcc65dd15bb79e0d0913ce11976204
Ali Gholami <aghce@aut.ac.ir>
2017-04-01 18:44:31 +0330

    see if the button is unpressed

--- ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
+++ ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
@@ -9,6 +9,7 @@
 ; Replace with your application code
 start:
     /* set the PINB0 data direction to 0 for input */
+	/* This one simulates the key */
 	ldi R16, 0x01	; Load 0b00000001 in R16
     out DDRB,R16	; Configure the PINB0 as input
 
@@ -21,13 +22,15 @@ OFF_MODE:
 	/* Put the PORTB to 0 */ 
 	ldi R18,0x00
 	out PORTB,R18
-	/* Check the content of PORTB0 */
-	ldi R17,0x01	; Load 0b00000001 in R16
-	cp PINB,R17		; Compare the contents to see if the button is pressed
+	/* Check the content of PINB0 */
+	ldi R19,0x01	; Load 0b00000001 in R19
+	cp PINB,R19		; Compare the contents to see if the button is pressed
 	brne OFF_MODE	; Branch to the OFF_MODE if the key isn't pressed yet
 ON_MODE:
 	/* Put the PORTB to 1 */
 	ldi R18,0xFF
 	out PORTB,R18
-
+	/* Check the content of PINB0 */
+	ldi R19,0x01	; Load 0b00000001 in R19
+	cp PINB,R19		; Compare the contents to see if the button is unpressed */
 	rjmp start
