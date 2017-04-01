22d69a017608b4acbf94010a99b81606edc458ef
Ali Gholami <aghce@aut.ac.ir>
2017-04-01 18:39:17 +0330

    Put the PORTB to 1 in ON_MODE

--- ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
+++ ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
@@ -21,10 +21,13 @@ OFF_MODE:
 	/* Put the PORTB to 0 */ 
 	ldi R18,0x00
 	out PORTB,R18
-	
 	/* Check the content of PORTB0 */
 	ldi R17,0x01	; Load 0b00000001 in R16
 	cp PINB,R17		; Compare the contents to see if the button is pressed
 	brne OFF_MODE	; Branch to the OFF_MODE if the key isn't pressed yet
+ON_MODE:
+	/* Put the PORTB to 1 */
+	ldi R18,0xFF
+	out PORTB,R18
 
 	rjmp start
