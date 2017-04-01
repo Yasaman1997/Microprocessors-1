8c6b8d126517ec35458b05495c1cd685d5984df8
Ali Gholami <aghce@aut.ac.ir>
2017-04-01 18:45:11 +0330

    Branch to the ON_MODE

--- ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
+++ ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
@@ -33,4 +33,5 @@ ON_MODE:
 	/* Check the content of PINB0 */
 	ldi R19,0x01	; Load 0b00000001 in R19
 	cp PINB,R19		; Compare the contents to see if the button is unpressed */
+	brne ON_MODE	; Branch to the ON_MODE if the key isn't unpressed yet
 	rjmp start
