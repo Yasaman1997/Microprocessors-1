6ddc14ab3e20f8c9b247c0f9c68c6746cf8e455a
Ali Gholami <aghce@aut.ac.ir>
2017-04-02 09:55:05 +0330

    Config the ports for two inputs and one output

--- ATmega Programming/WATCHDOG_TIMER/LED_WATCHDOG/LED_WATCHDOG.asm
+++ ATmega Programming/WATCHDOG_TIMER/LED_WATCHDOG/LED_WATCHDOG.asm
@@ -10,5 +10,21 @@
 
 ; Replace with your application code
 start:
-    inc r16
+    /* set the PINB0 data direction to 0 for input */
+	/* This one simulates the key */
+	ldi R16, 0x01	; Load 0b00000001 in R16
+    out DDRB,R16	; Configure the PINB0 as input
+
+	/* set the PINB1 data direction to 0 for input */
+	/* This one simulates the key */
+	/* This key will work as WDR */
+	clr R16			; Clear the R16
+	ldi R16, 0x02	; Load 0b00000020 in R16
+    out DDRB,R16	; Configure the PINB1 as input
+
+	/* set the PORTB data direction to 1 for output */
+	/* this one causes the LED to be ON/OFF */
+	ldi R17, 0x80	; Load 0b10000000 in R17
+	out DDRB,R17	; Configure the PORTB7 as output
+
     rjmp start
