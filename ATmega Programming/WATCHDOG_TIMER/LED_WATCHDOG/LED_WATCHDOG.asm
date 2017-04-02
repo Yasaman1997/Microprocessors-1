ccecdbb391c7d612512ac1f000f707579604f70c
Ali Gholami <aghce@aut.ac.ir>
2017-04-02 09:51:47 +0330

    LED watchdog clarification

--- ATmega Programming/WATCHDOG_TIMER/LED_WATCHDOG/LED_WATCHDOG.asm
+++ ATmega Programming/WATCHDOG_TIMER/LED_WATCHDOG/LED_WATCHDOG.asm
@@ -4,7 +4,9 @@
 ; Created: 4/2/2017 9:45:43 AM
 ; Author : Ali Gholami
 ;
-
+; A program to turn the LED on by pressing a key and wait for another key to prevent the WDT from reseting it
+; so it will keep alive all the time (button press must be less than 2.1s)
+; in case the LED goes off, we can enable it by pressing that first key 
 
 ; Replace with your application code
 start:
