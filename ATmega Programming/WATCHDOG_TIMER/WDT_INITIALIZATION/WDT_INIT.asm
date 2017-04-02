55faf02c1d72904be2c112a0fbda4418c7c4946c
Ali Gholami <aghce@aut.ac.ir>
2017-04-02 09:27:02 +0330

    add comments for clarification

--- ATmega Programming/WATCHDOG_TIMER/WDT_INITIALIZATION/WDT_INIT.asm
+++ ATmega Programming/WATCHDOG_TIMER/WDT_INITIALIZATION/WDT_INIT.asm
@@ -10,4 +10,16 @@
 ; Replace with your application code
 start:
     ; WDP2, WDP1, WDP0 must be confgure in order to use the 2.1 s timeout in wdt prescaling
+	; those bits are in WDTCR
+	; to use 2.1(s) timeout we need to put WDP2..WDP0 to "111" according to the data sheet
+	
+	/* Simply put 0 in a register */
+
+	/* Shift this register three times*/
+
+	/* Put the result in the WDTCR */
+
+	/* Now we have the initialized WDT for a 2.1(s) delay */
+
+
     rjmp start
