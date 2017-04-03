b592312def475a4573637541e61a89f4e0941ea6
Ali Gholami <aghce@aut.ac.ir>
2017-04-03 12:33:19 +0330

    A new way to compare

--- ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
+++ ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
@@ -23,19 +23,14 @@ OFF_MODE:
 	/* Put the PORTB7 to 0 */ 
 	ldi R18,(0 << PB7)
 	out PORTB,R18
-	/* Check the content of PINB0 */
-	ldi R19,0x01	; Load 0b00000001 in R19
-	//cp PINB,R19		; Compare the contents to see if the button is pressed
-	/* We cannot compare PIN or PORT registers with another Register from Regsiter file */
-	/* Just copy the PINB to R20 and compare R20 and R19 */
-	in R20,PINB
-	cp R20,R19
+	/* Skip if PIN 1 in PORT B is set */
+	sbis $B,0
 	brne OFF_MODE	; Branch to the OFF_MODE if the key isn't pressed yet
 ON_MODE:
 	/* Put the PORTB to 1 */
 	ldi R18,(1 << PB7)
 	out PORTB,R18
-	/* Skip if bit bit 1 in PORT B is set */
+	/* Skip if PIN bit 1 in PORT B is set */
 	sbis $B,7
 	brne ON_MODE	; Branch to the ON_MODE if the key isn't unpressed yet
 rjmp start
