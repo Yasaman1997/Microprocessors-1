23bd7592cfab192962efda6ac33d8ec3d200b4ad
Ali Gholami <aghce@aut.ac.ir>
2017-04-03 12:13:13 +0330

    Typo was fixed

--- ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
+++ ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
@@ -11,7 +11,7 @@ start:
     /* set the PINB0 data direction to 0 for input */
 	/* This one simulates the key */
 	cli
-	ldi R16, (0 << PD2)	; Make PD2 as input
+	ldi R16, (0 << PB2)	; Make PB2 as input
     out DDRB,R16	; Configure the PINB0 as input
 
 	/* set the PORTB7 data direction to 1 for output */
