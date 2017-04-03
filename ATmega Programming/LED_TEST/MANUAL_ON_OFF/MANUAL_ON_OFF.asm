7349290b213c63b157c3326884b751e437880a47
Ali Gholami <aghce@aut.ac.ir>
2017-04-03 21:02:17 +0330

    Standby mode

--- ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
+++ ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
@@ -5,6 +5,7 @@
 ; Author : Ali Gholami
 ;
 ; Program to test the LED ON/OFF states manually	
+; This LED will stay on standby mode when not pressing the key
 	start:
 		/* set the PIND2 data direction to 0 for input */
 		/* This one simulates the key */
@@ -24,6 +25,7 @@
 		sbis PIND,2
 		jmp OFF_MODE	; Branch to the OFF_MODE if the key isn't pressed yet
 
+		call delay
 		/* Make sure to clear the PIND2 before we continue */
 		cbi PIND,2
 	ON_MODE:
@@ -32,10 +34,10 @@
 		ldi R18,(1 << PB7)
 		out PORTB,R18
 		/* Skip if PIN 2 in PORT D is set */
-
 		sbis PIND,2
 		jmp ON_MODE	; Branch to the ON_MODE if the key isn't unpressed yet
 	rjmp start
+
 delay:
     ldi r16, 0xFF
 delay_loop_1:
