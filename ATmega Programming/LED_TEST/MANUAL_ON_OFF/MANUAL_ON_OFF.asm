fee17598d8012bf4c5c6ec160e2db7dfacda9bdc
Ali Gholami <aghce@aut.ac.ir>
2017-04-03 12:44:13 +0330

    Change input to PD because of Interrupt

--- ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
+++ ATmega Programming/LED_TEST/MANUAL_ON_OFF/MANUAL_ON_OFF.asm
@@ -11,29 +11,32 @@
 
 ISR_HERE:
 	cli
-	/* set the PINB0 data direction to 0 for input */
+	/* set the PIND0 data direction to 0 for input */
 	/* This one simulates the key */
-	ldi R16, (0 << PB2)	; Make PB2 as input
-    out DDRB,R16	; Configure the PINB0 as input
+	ldi R16, (0 << PD2)	; Make PD2 as input
+    out DDRB,R16	; Configure the PIND0 as input
+
 
 	/* set the PORTB7 data direction to 1 for output */
 	/* this one causes the LED to be ON/OFF */
 	ldi R17, (1 << PB7)	; Make PB7 as output 
 	out DDRB,R17	; Configure the PORTB7 as output
+
+
 	
 start:
 OFF_MODE:
 	/* Put the PORTB7 to 0 */ 
 	ldi R18,(0 << PB7)
 	out PORTB,R18
-	/* Skip if PIN 1 in PORT B is set */
-	sbis $B,0
+	/* Skip if PIN 1 in PORT D is set */
+	sbis $D,0
 	brne OFF_MODE	; Branch to the OFF_MODE if the key isn't pressed yet
 ON_MODE:
 	/* Put the PORTB to 1 */
 	ldi R18,(1 << PB7)
 	out PORTB,R18
-	/* Skip if PIN bit 1 in PORT B is set */
-	sbis $B,7
+	/* Skip if PIN 1 in PORT D is set */
+	sbis $D,7
 	brne ON_MODE	; Branch to the ON_MODE if the key isn't unpressed yet
 rjmp start
