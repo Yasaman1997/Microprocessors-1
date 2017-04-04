82b8bf7088f008b0167e16ed6b2d19ddc736a29e
Ali Gholami <aghce@aut.ac.ir>
2017-04-04 08:20:01 +0330

    Setup the stack before using ret instruction

--- ATmega Programming/LED_TEST/LED_10_TIME_BLINKER/LED_10_TIME_BLINKER.asm
+++ ATmega Programming/LED_TEST/LED_10_TIME_BLINKER/LED_10_TIME_BLINKER.asm
@@ -8,57 +8,58 @@
 
 ; Replace with your application code
 start:
+    //stack setup
+    ldi r16, 0
+    out SPH, r16
+    ldi r16, 0xf0
+    out SPL, r16
     /* set the PINB0 data direction to 0 for input */
-	/* This one simulates the key */
-	ldi R16, (0 << PB0)	; Load 0b00000001 in R16
-    out DDRB,R16	; Configure the PINB0 as input
+    /* This one simulates the key */
+    ldi R16, (0 << PB0) ; Load 0b00000001 in R16
+    out DDRB,R16    ; Configure the PINB0 as input
 
-	/* set the PORTB7 data direction to 1 for output */
-	/* this one causes the LED to be ON/OFF */
-	ldi R17, (1 << PB7)	; Load 0b10000000 in R17
-	out DDRB,R17	; Configure the PORTB7 as output
+    /* set the PORTB7 data direction to 1 for output */
+    /* this one causes the LED to be ON/OFF */
+    ldi R17, (1 << PB7) ; Load 0b10000000 in R17
+    out DDRB,R17    ; Configure the PORTB7 as output
 
 OFF_MODE:
-	/* Put the PORTB7 to 0 */ 
-	ldi R18,(0 << PB7)
-	out PORTB,R18
-	/* Check the content of PINB0 */
-	/* Wait for the PINB to get pressed by the user */
-	sbis PINB,0
-	brne OFF_MODE	; Branch to the OFF_MODE if the key isn't pressed yet
+    /* Put the PORTB7 to 0 */ 
+    ldi R18,(0 << PB7)
+    out PORTB,R18
+    /* Check the content of PINB0 */
+    /* Wait for the PINB to get pressed by the user */
+    sbis PINB,0
+    brne OFF_MODE   ; Branch to the OFF_MODE if the key isn't pressed yet
+//Here was mistake
+    ldi R20, 10
 
-	    ldi R20, 10
 BLINK_MODE:
-	/* Define a counter */
-	ldi R20,0
-	/* Turn on the LED */
-	/* Put the PORTB7 to 1 */
-	ldi R18,(1 << PB7)
-	out PORTB,R18
+    /* Turn on the LED */
+    /* Put the PORTB7 to 1 */
+    ldi R18,(1 << PB7)
+    out PORTB,R18
 
-	/* Create a delay */
-	call LONG_DELAY
+    /* Create a delay */
+    call LONG_DELAY
 
-	/* Turn off the LED */
-	ldi R18,(0 << PB7)
-	out PORTB,R18
-	
-	/* dec the counter */
-	dec R20
-	
-	/* Check the content of the counter */
-	cpi R20,0x0A
-	brne BLINK_MODE
-	/* Clear the input to avoid duplicate press virtualization */
-	cbi PINB,0
-	jmp	OFF_MODE
+    /* Turn off the LED */
+    ldi R18,(0 << PB7)
+    out PORTB,R18
+
+    /* Increment the counter */
+    dec R20
+    brne BLINK_MODE
+    /* Clear the input to avoid duplicate press virtualization */
+    cbi PINB,0
+    jmp OFF_MODE
 rjmp start
 
-	/* Delay function */
+    /* Delay function */
 LONG_DELAY:
-	ldi r25,10
+    ldi r25,10
 LOOP:
-	nop
-	dec R25
-	brne LOOP
-ret
+    dec R25
+//here was redundant instruction 
+    brne LOOP
+    ret
