028f5d50f50298d380b4e2f2bd25c371dec13c79
Ali Gholami <aghce@aut.ac.ir>
2017-04-05 09:12:14 +0330

    Change the style of initializing the PORTB

--- ATmega Programming/7_Segment_Connection/7SegmentConnection.asm
+++ ATmega Programming/7_Segment_Connection/7SegmentConnection.asm
@@ -13,7 +13,7 @@ start:
 
 	/* Put the direction of port B to output */
 	/* to do this we use DDR(Data Direction Register) and turn all ports to 1 */
-	ldi R16,0xFF	; Load 0b11111111 in R16
+	ldi R16,(1 << PB0)|(1 << PB1)|(1 << PB2)|(1 << PB3)|(1 << PB4)|(1 << PB5)|(1 << PB6)|(1 << PB7) ; Load 0b11111111 in R16
     out DDRB,R16	; Configure the PORT B as output 
 
 	/* To reserve some place for the 7Segment encoding table in the memory
@@ -23,5 +23,5 @@ start:
 	* The DB directive takes a list of expressions,
 	* and must contain at least one expression. The DB directive must be placed in a Code Segment or an EEPROM Segment. */
 	.CSEG
-	BCDTo7-Seg: .DB 0xCF, 0xA4, 0xB0, 0x00, 0x99, 0x92, 0x82, 0xF8, 0x80
+	sevenSegEncodings: .DB 0xCF, 0xA4, 0xB0, 0x00, 0x99, 0x92, 0x82, 0xF8, 0x80
     rjmp start		
