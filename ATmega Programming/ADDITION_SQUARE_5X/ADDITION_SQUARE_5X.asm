3174a3f1243605cebd8ca896f4d25d20c2809043
Ali Gholami <aghce@aut.ac.ir>
2017-04-05 21:32:53 +0330

    Remove $ and pay attention to the hex values

--- ATmega Programming/ADDITION_SQUARE_5X/ADDITION_SQUARE_5X.asm
+++ ATmega Programming/ADDITION_SQUARE_5X/ADDITION_SQUARE_5X.asm
@@ -6,10 +6,10 @@
 ;
 start:
 	/* The number n must be stored in R10 according to the problem definition */ 
-	ldi R25,$25
+	ldi R25,25
 	mov R10,R25
 	/* Register R21 will be used as a immediate container */
-	ldi R21,$5
+	ldi R21,5
 	/* Find the square of the number */ 
 	/* R20 will start this procedure from 0 and will be added 5 more each time */
 	ldi R20,0
@@ -28,4 +28,5 @@ adder_subroutine:
 	/* Put the result back to the R1:R0 */
 	mov R1,R4
 	mov R0,R3
+	.EXIT
   rjmp start
