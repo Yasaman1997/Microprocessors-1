d49402f00f0e3fb19f0122ea0c7001812c7e69e1
Ali Gholami <aghce@aut.ac.ir>
2017-04-06 23:20:55 +0330

    stack setup was added

--- ATmega Programming/ARRAY SORTING/IN_DATA_MEMORY_SORT/ARRAY_SORT_ASM.asm
+++ ATmega Programming/ARRAY SORTING/IN_DATA_MEMORY_SORT/ARRAY_SORT_ASM.asm
@@ -8,10 +8,17 @@
 		.CSEG	; write to the program memory 
 	ARRAY: .DB 1, 5, 4, 6, 2, 8, 7, 4, 9, 3	; The stored numbers in program memory
 		.def flashsize = R16	; size for the elements block in the flash memory
+	/* Setup the stack */
+    ldi r16, 0
+    out SPH, r16
+    ldi r16, 0xf0
+    out SPL, r16
+
 start:
 
     rjmp start
 
+	/* Copy the data to the ram */
 flash2ram:
 	lpm 
 	st Y+,R0
