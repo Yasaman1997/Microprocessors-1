a33b0736798e37cec52887ee47471bf951647677
Ali Gholami <aghce@aut.ac.ir>
2017-04-06 23:17:43 +0330

    flashsize was added

--- ATmega Programming/ARRAY SORTING/IN_DATA_MEMORY_SORT/ARRAY_SORT_ASM.asm
+++ ATmega Programming/ARRAY SORTING/IN_DATA_MEMORY_SORT/ARRAY_SORT_ASM.asm
@@ -7,7 +7,7 @@
 
 		.CSEG	; write to the program memory 
 	ARRAY: .DB 1, 5, 4, 6, 2, 8, 7, 4, 9, 3	; The stored numbers in program memory
-
+		.def flashsize = R16	; size for the elements block in the flash memory
 start:
 
     rjmp start
