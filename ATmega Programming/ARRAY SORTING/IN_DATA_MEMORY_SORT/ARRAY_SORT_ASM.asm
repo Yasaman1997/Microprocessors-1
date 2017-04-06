bc7f7eacbc99e7936c3f80338ed626ad5d6165fd
Ali Gholami <aghce@aut.ac.ir>
2017-04-06 23:09:17 +0330

    write data to the program memory

---
+++ ATmega Programming/ARRAY SORTING/IN_DATA_MEMORY_SORT/ARRAY_SORT_ASM.asm
@@ -0,0 +1,13 @@
+;
+; ARRAY_SORT_2.asm
+;
+; Created: 4/6/2017 11:07:37 PM
+; Author : Ali Gholami
+;
+
+		.CSEG	; write to the program memory 
+	ARRAY: .DB 1, 5, 4, 6, 2, 8, 7, 4, 9, 3	; The stored numbers in program memory
+
+start:
+
+    rjmp start
