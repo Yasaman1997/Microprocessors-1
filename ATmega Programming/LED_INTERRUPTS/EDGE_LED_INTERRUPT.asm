0b07a80018ff5638916fc030bcc7553dc407d241
Ali Gholami <aghce@aut.ac.ir>
2017-04-13 20:23:35 +0330

    Interrupt config: IVSEL = 0

--- ATmega Programming/LED_INTERRUPTS/EDGE_LED_INTERRUPT.asm
+++ ATmega Programming/LED_INTERRUPTS/EDGE_LED_INTERRUPT.asm
@@ -4,9 +4,16 @@
 ; Created: 4/13/2017 6:07:36 AM
 ; Author : Ali Gholami
 ;
+; A program to turn the LED on/off by pressing the SW1 button
 
+; Configuration: IVSEL = 0, BOOTRST = 0
+CONFIG_INT:
+	; Make sure that the IVSEL is set to 0
+	ldi R16,(0 << IVCE)
+	out GICR,R16
+	ldi R16,(0 << IVSEL)
+	out GICR,R16
 
-; Replace with your application code
 start:
-    inc r16
+	
     rjmp start
