7f415e26fa1e981558d271789057942065f14b00
Ali Gholami <aghce@aut.ac.ir>
2017-04-14 16:03:56 +0330

    Identifying keypress number in the keyboard

---
+++ ATmega Programming/MATRIX_KEYBOARD/_SETUP/MATRIX_SETUP.asm
@@ -0,0 +1,13 @@
+;
+; MATRIX_SETUP.asm
+;
+; Created: 4/14/2017 4:01:22 PM
+; Author : Ali Gholami
+;
+
+
+; I'll setup a keyboard matrix in this file
+; This code is written to identify the pressed number in the keyboard
+
+start:
+    rjmp start
