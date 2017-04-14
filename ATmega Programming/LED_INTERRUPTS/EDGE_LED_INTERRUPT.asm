8ef81a989596eaa24316986f1dce84816c46e738
Ali Gholami <aghce@aut.ac.ir>
2017-04-14 08:31:31 +0330

    refactor the whole logic

--- ATmega Programming/LED_INTERRUPTS/EDGE_LED_INTERRUPT.asm
+++ ATmega Programming/LED_INTERRUPTS/EDGE_LED_INTERRUPT.asm
@@ -7,26 +7,56 @@
 ; A program to turn the LED on/off by pressing the SW1 button
 
 ; Reserved 2 bytes: jump to reset at the beginnning
-	jmp RESET
+.org 0x00
+	jmp RESET_ISR
+
 ; Configuration: Put the interrupt 1 vector at address $002
-.org $002
-CONF_INT_VEC:
+.org 0x02
 	jmp EXT_INT1
 
-; Main program start point
-.org $1C00
-RESET:
+EXT_INT1:
+	; Turn on the LED here, set PD7 as output for LED
+	ldi R16,(1 << PD7)
+	out DDRD,R16
+	out PORTD,R16
+	
+	; Control the data output, Keep the LED on untill another key-press interrupt comes in
+KEEP_ON:
+	sbic PIND,3
+	jmp KEEP_ON
+	
+	; Create a delay
+	call SHORT_DELAY
+
+	; Otherwise go to the KEEP_OFF
+	ldi R16,(0 << PD7)
+	out PORTD,R16
+
+KEEP_OFF:
+	sbic PIND,3
+	jmp KEEP_OFF
+	
+	; Create a delay
+	call SHORT_DELAY
+
+	; Otherwise, go out of interrupt
+	ret
+
+RESET_ISR:
 	; Set stack pointer to the top of ram 
 	ldi R16,high(RAMEND)
 	out SPH,R16
 	ldi R16,low(RAMEND)
 	out SPL,R16
 
-	; Enable interrupts
+	; Enable Global interrupt flag
 	sei
 
+	; Enable INT1
+	ldi r16, (1 << INT1) 
+	out GICR, r16
+
 	; Configuration: IVSEL = 0, BOOTRST = 0
-CONF_INT:
 	; Make sure that the IVSEL is set to 0
 	ldi R16,(0 << IVCE)
 	out GICR,R16
@@ -37,49 +67,11 @@ start:
 	; Enable the input direction for PD3
 	ldi R16,(0 << PD3)
 	out DDRD,R16
-
-	; Check for any interrupts from SW1 (Which is connected to PD3)
-INT_CHECK:
-	sbis PIND,3
-	jmp INT_CHECK
-
-	; Create a delay
-	call SHORT_DELAY
-	; In case we detected any interrupts, jump to CONF_INT_VEC, then EXT_INT1 
-	jmp CONF_INT_VEC
     rjmp start
-
-EXT_INT1:
-	; Turn on the LED here, set PD7 as output for LED
-	ldi R16,(1 << PD7)
-	out DDRD,R16
-	out PORTD,R16
-	
-	; Control the data output, Keep the LED on untill another key-press interrupt comes in
-
-KEEP_ON:
-	sbis PIND,3
-	jmp KEEP_ON
-	
-	; Create a delay
-	call SHORT_DELAY
-
-	; Otherwise go to the KEEP_OFF
-	ldi R16,(0 << PD7)
-	out PORTD,R16
-KEEP_OFF:
-	sbis PIND,3
-	jmp KEEP_OFF
-	
-	; Create a delay
-	call SHORT_DELAY
-
-	; Otherwise, go out of interrupt
-	reti
 
 	; A short delay for synchronization between press and reaction
 SHORT_DELAY:
-    ldi r25,2
+    ldi r25,5
 LOOP:
     dec R25
     brne LOOP
