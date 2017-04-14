9e4eb22a699932b536f23ff75f21958601fbbfc0
Ali Gholami <aghce@aut.ac.ir>
2017-04-14 21:21:50 +0430

    jump to reset at the beginnning

--- ATmega Programming/MATRIX_KEYBOARD/_SETUP/MATRIX_SETUP.asm
+++ ATmega Programming/MATRIX_KEYBOARD/_SETUP/MATRIX_SETUP.asm
@@ -9,5 +9,9 @@
 ; I'll setup a keyboard matrix in this file
 ; This code is written to identify the pressed number in the keyboard
 
+; Reserved 2 bytes: jump to reset at the beginnning
+.org 0x00
+	jmp RESET_ISR
+
 start:
     rjmp start
