6c0da4f1f0f584dd0016d9f282b7e223c44e7d2e
Ali Gholami <aghce@aut.ac.ir>
2017-04-15 12:48:04 +0430

    adding a program to display the previos result

---
+++ ATmega Programming/MATRIX_KEYBOARD/_7Segment/MATRIX_7SEG_DISPLAY.asm
@@ -0,0 +1,12 @@
+;
+; MATRIX_7SEG_DISPLAY.asm
+;
+; Created: 4/15/2017 12:45:18 PM
+; Author : Ali Gholami
+;
+
+
+; A program to find the number of each button press in the matrix keyboard and display the numbers on the 7 Segment
+start:
+ 
+    rjmp start
