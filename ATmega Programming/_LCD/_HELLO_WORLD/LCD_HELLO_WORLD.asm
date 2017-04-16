b859241fb51a07d8e868c7b7e7e8ee29f4cc714e
Ali Gholami <aghce@aut.ac.ir>
2017-04-16 05:03:01 +0430

    Set the PA direction to output for LCD usage

--- ATmega Programming/_LCD/_HELLO_WORLD/LCD_HELLO_WORLD.asm
+++ ATmega Programming/_LCD/_HELLO_WORLD/LCD_HELLO_WORLD.asm
@@ -9,5 +9,9 @@
 ; A program to write the "HELLO WORLD" string on the LCD
 ; The LCD connection will be done using some libraries
 
+
+; Set the PA direction to output for LCD usage
+	ldi R16, (1 << PA0) | (1 << PA1) | (1 << PA2) | (1 << PA3) | (1 << PA4) | (1 << PA5) | (1 << PA6) | (1 << PA7)
+	out DDRA, R16
 start:
     rjmp start
