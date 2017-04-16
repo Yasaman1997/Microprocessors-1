d72274c0af3ffb7f9e8b1027eeab0beb777cb95c
Ali Gholami <aghce@aut.ac.ir>
2017-04-16 22:18:29 +0430

    include file type must be given in include section :|

--- ATmega Programming/_LCD/_HELLO_WORLD_MEM/HELLO_WORLD_MEM.asm
+++ ATmega Programming/_LCD/_HELLO_WORLD_MEM/HELLO_WORLD_MEM.asm
@@ -5,7 +5,7 @@
 ; Author : Ali Gholami
 ;
 ; A program to show the grabbed data from an specific array in the flash and show them on the LCD
-.include "m16_LCD_4bit"
+.include "m16_LCD_4bit.inc"
 ; Write an array of data in the flash section(code section)
 .CSEG
 LCDTABLE: .DB 14, 'M', 'Y', 'N', 'A', 'M', 'E', 'I', 'S', 'M', 'E', 'T', 'H', 'O', 'S'
