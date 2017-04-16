5bbc114eb4eb59875e0b4fb7555d1d1236e2d12d
Ali Gholami <aghce@aut.ac.ir>
2017-04-16 22:02:26 +0430

    the new program definition was added

---
+++ ATmega Programming/_LCD/_HELLO_WORLD_MEM/HELLO_WORLD_MEM.asm
@@ -0,0 +1,12 @@
+;
+; HELLO_WORLD_MEM.asm
+;
+; Created: 4/16/2017 9:59:41 PM
+; Author : Ali Gholami
+;
+
+
+; A program to show the grabbed data from an specific array in the flash and show them on the LCD
+start:
+   
+    rjmp start
