e59aabab71698fdd1f91a3b8f98a3cadad8b5e8c
Ali Gholami <aghce@aut.ac.ir>
2017-04-16 04:58:26 +0430

    adding a program to diplay hello world on the lcd

---
+++ ATmega Programming/_LCD/_HELLO_WORLD/LCD_HELLO_WORLD.asm
@@ -0,0 +1,13 @@
+;
+; LCD_HELLO_WORLD.asm
+;
+; Created: 4/16/2017 4:55:08 AM
+; Author : Ali Gholami
+;
+
+
+; A program to write the "HELLO WORLD" string on the LCD
+; The LCD connection will be done using some libraries
+
+start:
+    rjmp start
