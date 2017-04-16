15f3e644bec79e8dc9897c66114b244e6cce0f17
Ali Gholami <aghce@aut.ac.ir>
2017-04-16 22:15:48 +0430

    add header files

--- ATmega Programming/_LCD/_HELLO_WORLD_MEM/HELLO_WORLD_MEM.asm
+++ ATmega Programming/_LCD/_HELLO_WORLD_MEM/HELLO_WORLD_MEM.asm
@@ -4,10 +4,8 @@
 ; Created: 4/16/2017 9:59:41 PM
 ; Author : Ali Gholami
 ;
-
-
 ; A program to show the grabbed data from an specific array in the flash and show them on the LCD
-
+.include "m16_LCD_4bit"
 ; Write an array of data in the flash section(code section)
 .CSEG
 LCDTABLE: .DB 14, 'M', 'Y', 'N', 'A', 'M', 'E', 'I', 'S', 'M', 'E', 'T', 'H', 'O', 'S'
