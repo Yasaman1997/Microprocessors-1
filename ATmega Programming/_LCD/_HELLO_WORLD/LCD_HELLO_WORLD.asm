7f0ab7a773545204bb3b32c1a144aadd4a9fc9da
Ali Gholami <aghce@aut.ac.ir>
2017-04-16 06:26:15 +0430

    all the data lines to the lcd should be set to 1 for output purpose

--- ATmega Programming/_LCD/_HELLO_WORLD/LCD_HELLO_WORLD.asm
+++ ATmega Programming/_LCD/_HELLO_WORLD/LCD_HELLO_WORLD.asm
@@ -9,12 +9,13 @@
 ; The LCD connection will be done using some libraries
 .include "m16_LCD_4bit.inc"
 
-
-
 RESET_ISR:
 	; Set the PA direction to output for LCD usage
-	ldi R16, (1 << PA0) | (1 << PA1) | (1 << PA2) | (1 << PA3) | (1 << PA4) | (1 << PA5) | (1 << PA6) | (1 << PA7)
+	ldi R16,  (1 << PA4) | (1 << PA5) | (1 << PA6) | (1 << PA7)
 	out DDRA, R16
+
+
+
 
 start:
     rjmp start
