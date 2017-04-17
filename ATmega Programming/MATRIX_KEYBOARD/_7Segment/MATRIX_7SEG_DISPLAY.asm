3742263b403aebb93705789b40ffe8586af12db9
Ali Gholami <aghce@aut.ac.ir>
2017-04-17 19:18:23 +0430

    Pull up on port c

--- ATmega Programming/MATRIX_KEYBOARD/_7Segment/MATRIX_7SEG_DISPLAY.asm
+++ ATmega Programming/MATRIX_KEYBOARD/_7Segment/MATRIX_7SEG_DISPLAY.asm
@@ -7,7 +7,7 @@
 
 
 ; A program to find the number of each button press in the matrix keyboard and display the numbers on the 7 Segment
-; Simply store the codes needed for the 7 Segment in the EEPROM memory 
+; Simply store the codes needed for the 7 Segment in the EEPROM memory
 
 ; The first part of the code is the matrix setup from the previous section
 ; This code is written to identify the pressed number in the keyboard
@@ -32,12 +32,8 @@ HANDLE_MATRIX_PRESS:
 KEY_FIND:
 
 COL_FIND:
-	; Enable DDRC for LSBs of PC
-	ldi R16,(1 << PC0) | (1 << PC1) | (1 << PC2) | (1 << PC3) | (0 << PC4) | (0 << PC5) | (0 << PC6) | (0 << PC7)
-	out DDRC,R16
-
-	; Pull-Up for PORT C on bits 4-7
-	ldi r16, (0 << PC0) | (0 << PC1) | (0 << PC2) | (0 << PC3) | (1 << PC4) | (1 << PC5) | (1 << PC6) | (1 << PC7)
+	; Pull-Up for PORT C on bits 0-3
+	ldi r16, (1 << PC0) | (1 << PC1) | (1 << PC2) | (1 << PC3) | (0 << PC4) | (0 << PC5) | (0 << PC6) | (0 << PC7)
 	out PORTC, r16
 
 	; Find the column number
@@ -55,10 +51,10 @@ SET_COL_1:
 	jmp ROW_FIND
 SET_COL_2:
 	ldi col,2
-	jmp ROW_FIND	
+	jmp ROW_FIND
 SET_COL_3:
 	ldi col,3
-	jmp ROW_FIND	
+	jmp ROW_FIND
 SET_COL_4:
 	ldi col,4
 	jmp ROW_FIND
@@ -88,10 +84,10 @@ SET_ROW_1:
 	jmp CALCULATE_AND_RETURN
 SET_ROW_2:
 	ldi row,2
-	jmp CALCULATE_AND_RETURN	
+	jmp CALCULATE_AND_RETURN
 SET_ROW_3:
 	ldi row,3
-	jmp CALCULATE_AND_RETURN	
+	jmp CALCULATE_AND_RETURN
 SET_ROW_4:
 	ldi row,4
 	jmp CALCULATE_AND_RETURN
@@ -110,8 +106,8 @@ CALCULATE_AND_RETURN:
 RESET_ISR:
 	; Set E2_OFFSET = 0
 	ldi E2_OFFSET,0
-	
-	; Set stack pointer to the top of ram 
+
+	; Set stack pointer to the top of ram
 	ldi R16,high(RAMEND)
 	out SPH,R16
 	ldi R16,low(RAMEND)
@@ -122,7 +118,7 @@ RESET_ISR:
 	out MCUCR,R16
 
 	; Enable INT0
-	ldi R16, (1 << INT0) 
+	ldi R16, (1 << INT0)
 	out GICR, R16
 
 	; Configuration: IVSEL = 0, BOOTRST = 0
@@ -136,41 +132,33 @@ RESET_ISR:
 	sei
 
 start:
-	; Enable the input direction for PD2
-	ldi R16,(0 << PD2)
-	out DDRD,R16
-	; Listen for interrupt!
-	sbic PIND,2
-	call HANDLE_MATRIX_PRESS
-
-
 	; Ok :D When returned, we have the desired number in the R1
-	; Lets start showing it on the 7 Segment 
+	; Lets start showing it on the 7 Segment
 	; Convert the number into the normal mode
 	; Put the converted number in the SH_RS variable
 	movw SH_RS_H:SH_RS_L, R2:R1
 	; Compare the returned value with the immediate
 	cpi SH_RS_L, 17
 	adiw SH_RS_H:SH_RS_L, 0
-	
+
 	cpi SH_RS_L, 18
 	adiw SH_RS_H:SH_RS_L, 1
-	
+
 	cpi SH_RS_L, 19
 	adiw SH_RS_H:SH_RS_L, 2
-	
+
 	cpi SH_RS_L, 20
 	adiw SH_RS_H:SH_RS_L, 3
 
 	cpi SH_RS_L, 33
 	adiw SH_RS_H:SH_RS_L, 4
-	
+
 	cpi SH_RS_L, 34
 	adiw SH_RS_H:SH_RS_L, 5
-	
+
 	cpi SH_RS_L, 35
 	adiw SH_RS_H:SH_RS_L, 6
-	
+
 	cpi SH_RS_L, 49
 	adiw SH_RS_H:SH_RS_L, 7
 
