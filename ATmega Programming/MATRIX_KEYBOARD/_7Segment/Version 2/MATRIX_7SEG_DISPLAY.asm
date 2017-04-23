c148d4b5f45b98d6d2f33557fe78d47b2b721b71
Ali Gholami <aghce@aut.ac.ir>
2017-04-23 10:27:58 +0430

    init the stack pointer

--- ATmega Programming/MATRIX_KEYBOARD/_7Segment/Version 2/MATRIX_7SEG_DISPLAY.asm
+++ ATmega Programming/MATRIX_KEYBOARD/_7Segment/Version 2/MATRIX_7SEG_DISPLAY.asm
@@ -3,10 +3,30 @@
 ;
 ; Created: 4/23/2017 9:56:49 AM
 ; Author : Ali Gholami
-;
-
-
 ; This application will enable the interrupts of atmega by pressing each keypad button
+jmp RESET
+
+
+RESET:
+	; Init the stack pointer
+	ldi R16,low(RAMEND)
+	out SPL,R16
+	ldi R16,high(RAMEND)
+	out SPH,R16
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 start:
-    inc r16
     rjmp start
