2b12504bdcd9ee371e28edf667b1dc62e93cffb2
Ali Gholami <aligholami7596@gmail.com>
2017-05-07 18:57:19 +0430

    code improvements

--- ATmega Programming/TCT_PRACTICE/TC0_CTC.asm
+++ ATmega Programming/TCT_PRACTICE/TC0_CTC.asm
@@ -5,7 +5,8 @@
 ; Author : aligholamee
 ;
 ; This program will use the TCT0 to blink the LEDs using CTC mode
-.def temp = R16
+.def TEMP = R16
+.def GLOBAL_OVERFLOW_COUNTER = R17
 
 ; RESET Vector
 .org 0x00
@@ -15,36 +16,36 @@
 RESET_ISR:
 	
 	; Stack Init
-	ldi temp,HIGH(RAMEND)	
-	out SPH,temp
-	ldi temp,LOW(RAMEND)
-	out SPL,temp
+	ldi TEMP,HIGH(RAMEND)	
+	out SPH,TEMP
+	ldi TEMP,LOW(RAMEND)
+	out SPL,TEMP
 
 	; Configure the clock source for TC0
 	; The clock source is selected by the clock select logic which is controlled 
     ; by the clock select (CS02:0) bits located in the Timer/Counter Control Register (TCCR0)
-	ldi temp,(1 << CS02) | (0 << CS01) | (1 << CS00)
-	out TCNT0,temp
+	ldi TEMP,(1 << CS02) | (0 << CS01) | (1 << CS00)
+	out TCNT0,TEMP
 
 	; Configure the TC0 Mode
 	; Waveform Generation Mode Bits (WGM00 - WGM01)
 	; These bits control the counting sequence of the counter, the source for the maximum (TOP)
 	; counter value, and what type of Waveform Generation to be used
-	ldi temp,(0 << WGM00) | (0 << WGM01)
-	out TCCR0,temp
+	ldi TEMP,(0 << WGM00) | (0 << WGM01)
+	out TCCR0,TEMP
 
 	; Configure the COM Bits
 	; Since there are no compare matches going to happen in this program, 
 	; we will make sure they are 0 :)
 	/* MAKE SURE YOU DON'T OVERRIDE THE PREVIOUS TEMP VALUE */
-	ori temp,(0 << COM00) | (0 << COM01)
-	out TCCR0,temp
+	ori TEMP,(0 << COM00) | (0 << COM01)
+	out TCCR0,TEMP
 
 	; Set PD5 as output
-	ldi temp << PD5)
-	out DDRD,temp
+	ldi TEMP << PD5)
+	out DDRD,TEMP
 	; Set PD4 as output 
-	ldi temp,(1 << PD4)
-	out DDRD,temp
+	ldi TEMP,(1 << PD4)
+	out DDRD,TEMP
 start:
     rjmp start
