12e5d335f1ea48d95e35caa24d21d9168802604c
Ali Gholami <aligholami7596@gmail.com>
2017-05-13 22:51:30 +0430

    ACIE and ACIS bits

--- ATmega Programming/Analog Comparator/AC_HUMIDITY.asm
+++ ATmega Programming/Analog Comparator/AC_HUMIDITY.asm
@@ -25,9 +25,14 @@ RESET_ISR:
 	; Analog comparator control and status register (ACSR)
 	; Make sure ACD - Analog Comparator Disable is cleared
 	; Make sure ACBG - Analog Comparator Bandgap Select is cleared
-	; Make sure no Interrupts are enabled(done by default)
-	ldi TEMP,(0 << ACD)|(0 << ACBG)
+	; Make sure ACIE - Analog Comparator Interrupt Enable is enabled
+	; The interrupt mode has been set on the toggle mode
+	ldi TEMP,(0 << ACD)|(0 << ACBG)|(1 << ACIE)|(0 << ACIS1)|(0 << ACIS0)
 	out ACSR,TEMP
+
+	; LED1 - PD5 OUTPUT
+	ldi TEMP,(1 << PD5)
+	out DDRD,TEMP
 
 	; Global Interrupt Enable
 	sei
