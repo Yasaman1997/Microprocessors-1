251bd893533f750faee5d080221bbb0bf1eb817a
Ali Gholami <aligholami7596@gmail.com>
2017-05-15 17:33:42 +0430

    appropriate bits of REFSn in the ADMUX register

--- ATmega Programming/ADC/ADC_HUMIDITY/ADC_HUMIDITY.asm
+++ ATmega Programming/ADC/ADC_HUMIDITY/ADC_HUMIDITY.asm
@@ -24,6 +24,15 @@ RESET_ISR:
 	out SPH,TEMP
 	ldi TEMP,LOW(RAMEND)
 	out SPL,TEMP
+	
+	; I'll enable the needed bits for the analog to digital convertor in
+	; order to create an interrupt when the value goes between the MAX and MIN level
+	; 1 - Choose the reference by setting the appropriate bits of REFSn in the ADMUX register
+	ldi TEMP,(1 << REFS0)|(0 << REFS1)
+	out ADMUX,TEMP
+
+
+
 
 	; Global Interrupt Enable
 	sei
