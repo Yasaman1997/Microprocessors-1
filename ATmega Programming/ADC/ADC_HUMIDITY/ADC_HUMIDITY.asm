753037afddffe01e2bd3136dcba86e8fed0623c5
Ali Gholami <aligholami7596@gmail.com>
2017-05-16 12:56:09 +0430

    addm more temporary variables to handler adc

--- ATmega Programming/ADC/ADC_HUMIDITY/ADC_HUMIDITY.asm
+++ ATmega Programming/ADC/ADC_HUMIDITY/ADC_HUMIDITY.asm
@@ -5,6 +5,8 @@
 ; Author : aligholamee
 ;
 .include "m16_LCD_4bit.inc"
+.def TEMP2 = R19
+.def TEMP3 = R20
 ;======================MAIN=============================================
 start:
 	; stay here until a conversion completes
@@ -16,11 +18,11 @@ ADC_CC:
 	cli
 	; ADC Conversion Complete ISR
 	; Simply show the conversion result on the LCD
-	in TEMP,ADCL
-	mov argument,TEMP
+	in TEMP2,ADCL
+	in TEMP3,ADCH
+	mov argument,TEMP2
 	call lcd_putchar
-	in TEMP,ADCH
-	mov argument,TEMP
+	mov argument,TEMP3
 	call lcd_putchar
 	ret
 ;======================ADC_CC===========================================
