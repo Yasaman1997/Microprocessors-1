a0384cdffe6b894523e56e3dc624589f324f37a9
Ali Gholami <aligholami7596@gmail.com>
2017-05-16 10:56:15 +0430

    rename

--- ATmega Programming/ADC/ADC_HUMIDITY/ADC_HUMIDITY.asm
+++ ATmega Programming/ADC/ADC_HUMIDITY/ADC_HUMIDITY.asm
@@ -7,11 +7,11 @@
 .include "m16_LCD_4bit.inc"
 
 ;======================VECTORS==========================================
-.def TEMP = R16
+.def TEMP2 = R25
 
 ; RESET Vector
-.org $000
-	jmp RESET_ISR
+;.org $000
+	;jmp RESET_ISR
 
 ; ADC Conversion Complete Interrupt Vector
 .org $01C
@@ -22,30 +22,30 @@
 RESET_ISR:
 	
 	; Stack Init
-	ldi TEMP,HIGH(RAMEND)	
-	out SPH,TEMP
-	ldi TEMP,LOW(RAMEND)
-	out SPL,TEMP
+	ldi TEMP2,HIGH(RAMEND)	
+	out SPH,TEMP2
+	ldi TEMP2,LOW(RAMEND)
+	out SPL,TEMP2
 	
 	; I'll enable the needed bits for the analog to digital convertor in
 	; order to create an interrupt when the value goes between the MAX and MIN level
 	; 1 - Choose the reference by setting the appropriate bits of REFSn in the ADMUX register
-	ldi TEMP,(1 << REFS0)|(0 << REFS1)
-	out ADMUX,TEMP
+	ldi TEMP2,(1 << REFS0)|(0 << REFS1)
+	out ADMUX,TEMP2
 
 	; 2 - Choose whether the analog input channel and the differential gain by setting the MUX bits in the regsiter ADMUX
 	; Since the question is asking for an alarm in case the humidity of the environment is out of the domain,
 	; In this section, we only use the single channel mode for the ADC1 and convert the analog input to a digital output
-	in ADMUX,TEMP
-	ori TEMP,(0 <<  MUX4)|(0 << MUX3)|(0 << MUX2)|(0 << MUX1)|(1 << MUX0)
+	in ADMUX,TEMP2
+	ori TEMP2,(0 <<  MUX4)|(0 << MUX3)|(0 << MUX2)|(0 << MUX1)|(1 << MUX0)
 	out ADMUX
 
 	; 3 - The ADC is enabled by setting the ADC Enable bit, ADEN in ADCSRA. Voltage reference and
 	; input channel selections will not go into effect until ADEN is set
 	; Turn the ADC Interrupt on
 	; Keep the prescaler values same as the default
-	ldi TEMP,(1 << ADEN)|(1 << ADIE)
-	out ADCSRA,TEMP
+	ldi TEMP2,(1 << ADEN)|(1 << ADIE)
+	out ADCSRA,TEMP2
 
 	; Global Interrupt Enable
 	sei
