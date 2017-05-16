8caa6570722d91a85efeeac224e6dac73fb77526
Ali Gholami <aligholami7596@gmail.com>
2017-05-16 11:04:04 +0430

    mov vectors and reset body to the header file

--- ATmega Programming/ADC/ADC_HUMIDITY/ADC_HUMIDITY.asm
+++ ATmega Programming/ADC/ADC_HUMIDITY/ADC_HUMIDITY.asm
@@ -5,50 +5,10 @@
 ; Author : aligholamee
 ;
 .include "m16_LCD_4bit.inc"
-
-;======================VECTORS==========================================
-.def TEMP2 = R25
-
-; RESET Vector
-;.org $000
-	;jmp RESET_ISR
-
-; ADC Conversion Complete Interrupt Vector
-;.org $01C
-	jmp ADC_CC
-;======================VECTORS==========================================
-
 ;======================RESET_ISR========================================
 RESET_ISR:
 	
-	; Stack Init
-	ldi TEMP2,HIGH(RAMEND)	
-	out SPH,TEMP2
-	ldi TEMP2,LOW(RAMEND)
-	out SPL,TEMP2
-	
-	; I'll enable the needed bits for the analog to digital convertor in
-	; order to create an interrupt when the value goes between the MAX and MIN level
-	; 1 - Choose the reference by setting the appropriate bits of REFSn in the ADMUX register
-	ldi TEMP2,(1 << REFS0)|(0 << REFS1)
-	out ADMUX,TEMP2
 
-	; 2 - Choose whether the analog input channel and the differential gain by setting the MUX bits in the regsiter ADMUX
-	; Since the question is asking for an alarm in case the humidity of the environment is out of the domain,
-	; In this section, we only use the single channel mode for the ADC1 and convert the analog input to a digital output
-	in TEMP2,ADMUX
-	ori TEMP2,(0 <<  MUX4)|(0 << MUX3)|(0 << MUX2)|(0 << MUX1)|(1 << MUX0)
-	out ADMUX,TEMP2
-
-	; 3 - The ADC is enabled by setting the ADC Enable bit, ADEN in ADCSRA. Voltage reference and
-	; input channel selections will not go into effect until ADEN is set
-	; Turn the ADC Interrupt on
-	; Keep the prescaler values same as the default
-	ldi TEMP2,(1 << ADEN)|(1 << ADIE)
-	out ADCSRA,TEMP2
-
-	; Global Interrupt Enable
-	sei
 ;======================RESET_ISR========================================
 
 ;======================MAIN=============================================
