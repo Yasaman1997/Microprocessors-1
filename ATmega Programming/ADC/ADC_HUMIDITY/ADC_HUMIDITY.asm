3eab3cc6152251764ed15013b167d2f2c8b7fe12
Ali Gholami <aligholami7596@gmail.com>
2017-05-18 13:16:56 +0430

    refactor all files

--- ATmega Programming/ADC/ADC_HUMIDITY/ADC_HUMIDITY.asm
+++ ATmega Programming/ADC/ADC_HUMIDITY/ADC_HUMIDITY.asm
@@ -5,224 +5,252 @@
 ; Author : aligholamee
 ;
 
-
-.def adcLow=r21
-.def adcHigh=r22
-.def maxVL=r23
-.def maxVH=r24
-.def minVL=r25
-.def minVH=r26
-.def compRes=r27
-.def counter=r28
-.def counter2=r29
 .include "m16_LCD_4bit.inc"
-; Replace with your application code
+;======================DEF_SECTION=======================================
+.def ADC_LOW_BYTE=R21
+.def ADC_HIGH_BYTE=R22
+.def VOLT_REF1_LOW=R23
+.def VOLT_REF1_HIGH=R24
+.def VOLT_REF2_LOW=R25
+.def VOLT_REF2_HIGH=R26
+.def COMPARISON_RESULT=R27
+.def CNT1=R28
+.def CNT2=R29
+;======================DEF_SECTION=======================================
+
+;======================INT_VECTOR========================================
 .org 0x001c
-intrupt:
-	/*cpi r30,0x00
-	breq x1
-	ldi r30,0xff
-	rjmp x2
-	x1:ldi r30,0xff
-	x2:out portb,r30*/
-	ldi r16,(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADATE)|(1<<ADIE)
-	out ADCSRA,r16
+	jmp ADC_COMPLETE_ISR
+;======================INT_VECTOR========================================
+
+;======================INITIALIZE========================================
+INITIALIZE:
+	ldi CNT1,0x00
+	ldi R16,low(RAMEND)
+	out SPL,R16
+	ldi R16,high(RAMEND)
+	out SPH,R16
+	ldi R16,(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADATE)|(1<<ADIE)
+	out ADCSRA,R16
 	sei
-	rjmp continue
-start:
-init:
-	ldi counter,0x00
-	ldi r16,low(ramend)
-	out spl,r16
-	ldi r16,high(ramend)
-	out sph,r16
-	ldi r16,(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADATE)|(1<<ADIE)
-	out ADCSRA,r16
-	sei
-	ldi r16,0x00
-	out ddrb,r16
-	ldi r16,0xff
-	out ddrc,r16
-	call readMinSensor
-	call readMaxSensor
-	rjmp again
-again:
-	call readHSensor
-	call compHigh
-	cpi compRes,0x01
-	breq light1On
-	call compLow
-	cpi compRes,0x00
-	breq lightOff
-	cpi compRes,0x02
-	breq light2On
-light1On:
-	ldi r16,0x01
-	out portc,r16
-	rjmp end
-light2On:
-	ldi r16,0x02
-	out portc,r16
-	rjmp end
-lightOff:
-	ldi r16,0x00
-	out portc,r16
-	rjmp end
-compHigh:
-	cp adcHigh,maxVH
-	brlo normalH
-	cp adcLow,maxVL
-	brlo normalH
-	ldi  compRes,0x01
+	ldi R16,0x00
+	out DDRB,R16
+	ldi R16,0xff
+	out DDRC,R16
+	call REF2_ADC_ANALYZE
+	call REF1_ADC_ANALYZE
+	jmp RE_COMPARE
+;======================INITIALIZE========================================
+
+;======================RE_COMPARE========================================
+RE_COMPARE:
+	call GENERAL_ADC_ANALYZE
+	call COMPARE_REF_1
+	cpi COMPARISON_RESULT,0x01
+	breq LED_1_ON_MODE
+	call COMPARE_REF_2
+	cpi COMPARISON_RESULT,0x00
+	breq LED_OFF_MODE
+	cpi COMPARISON_RESULT,0x02
+	breq LED_2_ON_MODE
+LED_1_ON_MODE:
+	ldi R16,0x01
+	out portc,R16
+	jmp END_OF_ROUTINE
+LED_2_ON_MODE:
+	ldi R16,0x02
+	out portc,R16
+	jmp END_OF_ROUTINE
+LED_OFF_MODE:
+	ldi R16,0x00
+	out portc,R16
+	jmp END_OF_ROUTINE
+COMPARE_REF_1:
+	cp ADC_HIGH_BYTE,VOLT_REF1_HIGH
+	brlo GENERALIZE_1
+	cp ADC_LOW_BYTE,VOLT_REF1_LOW
+	brlo GENERALIZE_1
+	ldi  COMPARISON_RESULT,0x01
 	ret
-normalH:
-	ldi compRes,0x00
+GENERALIZE_1:
+	ldi COMPARISON_RESULT,0x00
 	ret
-compLow:
-	cp minVH,adcHigh
-	brlo normalL
-	cp minVL,adcLow
-	brlo normalL
-	ldi  compRes,0x02
+COMPARE_REF_2:
+	cp VOLT_REF2_HIGH,ADC_HIGH_BYTE
+	brlo GENERALIZE_2
+	cp VOLT_REF2_LOW,ADC_LOW_BYTE
+	brlo GENERALIZE_2
+	ldi  COMPARISON_RESULT,0x02
 	ret
-normalL:
-	ldi compRes,0x00
+GENERALIZE_2:
+	ldi COMPARISON_RESULT,0x00
 	ret
-end:
-	inc counter
-	cpi counter,0xff
-	breq showLed
-	rjmp again
-showLed:
-	CALL LCD_INIT
-	CALL LCD_WAIT
-	ldi counter,0x00
-;	call reset
-;	cpi lcdhigh,0x03
-;	brlo show3
-part2:
-	cpi adcLow,0x0a
-	brlo show10
-	cpi adcLow,0x14
-	brlo show20
-	cpi adcLow,0x1e
-	brlo show30
-	cpi adcLow,0x28
-	brlo show40
-	cpi adcLow,0x32
-	brlo show50
-	cpi adcLow,0x3c
-	brlo show60
-	cpi adcLow,0x46
-	brlo show70
-	cpi adcLow,0x50
-	brlo show80
-	cpi adcLow,0x64
-	brlo show90
-end2:
-	rjmp again
-show10:
+END_OF_ROUTINE:
+	inc CNT1
+	cpi CNT1,0xff
+	breq DISPLAY_ON_LCD
+	jmp RE_COMPARE
+;======================RE_COMPARE========================================
+
+;======================DISPLAY_ON_LCD====================================
+DISPLAY_ON_LCD:
+	call LCD_INIT
+	call LCD_WAIT
+	ldi CNT1,0x00
+;======================DISPLAY_ON_LCD====================================
+
+;======================INTEGER_CONVERSION================================
+INTEGER_CONVERSION:
+	cpi ADC_LOW_BYTE,0x0a
+	brlo DISPLAY_ON_LCD10
+	cpi ADC_LOW_BYTE,0x14
+	brlo DISPLAY_ON_LCD20
+	cpi ADC_LOW_BYTE,0x1e
+	brlo DISPLAY_ON_LCD30
+	cpi ADC_LOW_BYTE,0x28
+	brlo DISPLAY_ON_LCD40
+	cpi ADC_LOW_BYTE,0x32
+	brlo DISPLAY_ON_LCD50
+	cpi ADC_LOW_BYTE,0x3c
+	brlo DISPLAY_ON_LCD60
+	cpi ADC_LOW_BYTE,0x46
+	brlo DISPLAY_ON_LCD70
+	cpi ADC_LOW_BYTE,0x50
+	brlo DISPLAY_ON_LCD80
+	cpi ADC_LOW_BYTE,0x64
+	brlo DISPLAY_ON_LCD90
+;======================INTEGER_CONVERSION================================
+
+END_OF_ROUTINE2:
+	jmp RE_COMPARE
+
+;======================DISPLAY_ON_LCD====================================
+DISPLAY_ON_LCD10:
 	ldi argument,'1'
 	call lcd_putchar
 	ldi argument,'0'
 	call lcd_putchar
-	rjmp end2
+	jmp END_OF_ROUTINE2
 
-show20:
+DISPLAY_ON_LCD20:
 	ldi argument,'2'
 	call lcd_putchar
 	ldi argument,'0'
 	call lcd_putchar
-	rjmp end2
+	jmp END_OF_ROUTINE2
 
-show30:
+DISPLAY_ON_LCD30:
 	ldi argument,'3'
 	call lcd_putchar
 	ldi argument,'0'
 	call lcd_putchar
-	rjmp end2
+	jmp END_OF_ROUTINE2
 
-show40:
+DISPLAY_ON_LCD40:
 	ldi argument,'4'
 	call lcd_putchar
 	ldi argument,'0'
 	call lcd_putchar
-	rjmp end2
+	jmp END_OF_ROUTINE2
 
-show50:
+DISPLAY_ON_LCD50:
 	ldi argument,'5'
 	call lcd_putchar
 	ldi argument,'0'
 	call lcd_putchar
-	rjmp end2
+	jmp END_OF_ROUTINE2
 
-show60:
+DISPLAY_ON_LCD60:
 	ldi argument,'6'
 	call lcd_putchar
 	ldi argument,'0'
 	call lcd_putchar
-	rjmp end2
+	jmp END_OF_ROUTINE2
 
-show70:
+DISPLAY_ON_LCD70:
 	ldi argument,'7'
 	call lcd_putchar
 	ldi argument,'0'
 	call lcd_putchar
-	rjmp end2
+	jmp END_OF_ROUTINE2
 
-show80:
+DISPLAY_ON_LCD80:
 	ldi argument,'8'
 	call lcd_putchar
 	ldi argument,'0'
 	call lcd_putchar
-	rjmp end2
+	jmp END_OF_ROUTINE2
 
-show90:
+DISPLAY_ON_LCD90:
 	ldi argument,'9'
 	call lcd_putchar
 	ldi argument,'0'
 	call lcd_putchar
-	rjmp end2
+	jmp END_OF_ROUTINE2
+;======================DISPLAY_ON_LCD====================================
 
-wait:
-	ldi r16,(1<<ADSC)
-	in r17,ADCSRA;ADSC
-	and r16,r17
-	cpi r16,(1<<ADSC)
-	breq wait
+;======================WAIT HERE==========================================
+WAIT_HERE:
+	ldi R16,(1<<ADSC)
+	in R17,ADCSRA;ADSC
+	and R16,R17
+	cpi R16,(1<<ADSC)
+	breq WAIT_HERE
 	ret
-readHSensor:
-	ldi r16,0x00
-    out ADMUX,r16
-	in r16,ADCSRA
-	ori r16,(1<<ADSC)
-	out ADCSRA,r16
-	in r16,MCUCR
-	ori r16,(1<<SE)|(1<<SM0)|(0<<SM1)|(0<<SM2)
-	out MCUCR,r16
+;======================WAIT HERE=========================================
+
+
+;================MAIN SENSOR ADC CONVERSION + SLEEP MODE HERE============
+GENERAL_ADC_ANALYZE:
+	ldi R16,0x00
+    out ADMUX,R16
+	in R16,ADCSRA
+	ori R16,(1<<ADSC)
+	out ADCSRA,R16
+	in R16,MCUCR
+	ori R16,(1<<SE)|(1<<SM0)|(0<<SM1)|(0<<SM2)
+	out MCUCR,R16
 	sleep
-	continue:
-		in adcLow,ADCL
-		in adcHigh,ADCH
+;================MAIN SENSOR ADC CONVERSION + SLEEP MODE HERE============
+
+;======================RETURN HERE=========================================
+RETURN_HERE:
+		in ADC_LOW_BYTE,ADCL
+		in ADC_HIGH_BYTE,ADCH
 		ret
-readMaxSensor:
-	ldi r16,0x01
-    out ADMUX,r16
-	in r16,ADCSRA
-	ori r16,(1<<ADSC)
-	out ADCSRA,r16
-	call wait
-	in maxVL,ADCL
-	in maxVH,ADCH
-	out portc,maxVL
+;======================RETURN HERE=========================================
+
+;======================REF1_ADC_ANALYZE====================================
+REF1_ADC_ANALYZE:
+	ldi R16,0x01
+    out ADMUX,R16
+	in R16,ADCSRA
+	ori R16,(1<<ADSC)
+	out ADCSRA,R16
+	call WAIT_HERE
+	in VOLT_REF1_LOW,ADCL
+	in VOLT_REF1_HIGH,ADCH
+	out portc,VOLT_REF1_LOW
 	ret
-readMinSensor:
-	ldi r16,0x02
-    out ADMUX,r16
-	in r16,ADCSRA
-	ori r16,(1<<ADSC)
-	out ADCSRA,r16
-	call wait
-	in minVL,ADCL
-	in minVH,ADCH
+;======================REF1_ADC_ANALYZE====================================
+
+;======================REF2_ADC_ANALYZE====================================
+REF2_ADC_ANALYZE:
+	ldi R16,0x02
+    out ADMUX,R16
+	in R16,ADCSRA
+	ori R16,(1<<ADSC)
+	out ADCSRA,R16
+	call WAIT_HERE
+	in VOLT_REF2_LOW,ADCL
+	in VOLT_REF2_HIGH,ADCH
 	ret
+;======================REF2_ADC_ANALYZE====================================
+
+;======================ADC Conversion Complete ISR=========================
+ADC_COMPLETE_ISR:
+	ldi R16,(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADATE)|(1<<ADIE)
+	out ADCSRA,R16
+	sei
+	jmp RETURN_HERE
+;======================ADC Conversion Complete ISR=========================
