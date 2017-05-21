58228750e5a3d4de567dfc5989ef3927ec0eec3f
Ali Gholami <aligholami7596@gmail.com>
2017-05-21 23:53:37 +0430

    Configure the clock generation mode

--- ATmega Programming/USART/USART_ASM.asm
+++ ATmega Programming/USART/USART_ASM.asm
@@ -6,3 +6,20 @@
 ;
 ; Program to connect the atmega16 with the virtual DB9 port through MAX232
 
+.def TEMP = R16
+;======================VECTORS==========================================
+.org 0x00
+	jmp RESET_ISR
+
+
+;======================VECTORS==========================================
+
+;======================RESET_ISR========================================
+RESET_ISR:
+	; Configure the clock generation mode
+	ldi TEMP,(0 << UMSEL)
+	out UCSRC,TEMP
+
+;======================RESET_ISR========================================
+
+
