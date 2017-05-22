e9084dc5820e8636124f9ae2f1e46f15e7301372
Ali Gholami <aligholami7596@gmail.com>
2017-05-22 22:50:08 +0430

    Enable USART send and recieve

--- ATmega Programming/USART/USART_ASM.asm
+++ ATmega Programming/USART/USART_ASM.asm
@@ -7,6 +7,8 @@
 ; Program to connect the atmega16 with the virtual DB9 port through MAX232
 
 .def TEMP = R16
+.def F_OSC = R17
+.def 
 ;======================VECTORS==========================================
 .org 0x00
 	jmp RESET_ISR
@@ -23,11 +25,17 @@ RESET_ISR:
 	; Configure the data bits = 8 bits for data
 	ldi TEMP,(1 << UPM0)|(1 << UPM0)|(0 << UMSEL)|(1 << USBS)|(1 << UCSZ0)|(1 << UCSZ1)
 	out UCSRC,TEMP
-	ldi TEMP,(0 << UCSZ2)
+	; Enable USART send and recieve
+	ldi TEMP,(0 << UCSZ2)|(1 << RXEN)|(1 << TXEN)
 	out UCSRB,TEMP
-
-
 
 ;======================RESET_ISR========================================
 
+
+
+;======================MAIN PROGRAM=====================================
+start:
+	rjmp start
+
+;======================MAIN PROGRAM=====================================
 
