f2530b7663f7b08ded79849138a5251d7791bb66
Ali Gholami <aligholami7596@gmail.com>
2017-06-05 14:11:22 +0430

    add interrupt vectors

--- ATmega Programming/BUFFER_LATCH/BUF_LATCH_DEC.asm
+++ ATmega Programming/BUFFER_LATCH/BUF_LATCH_DEC.asm
@@ -7,3 +7,17 @@
 ; A program to connect 3 interact between the input of the buffers and 7-segments that are connected to the latches
 ; On each 7-seg, we will show the number of pressed key according to the buffer that has got the interrupt
 
+;============== DEFS ==============
+.def TEMP = R16
+;============== DEFS ==============
+
+;============= VECTORS ============
+.org 0x00
+	rjmp RESET_ISR
+.org 0x02
+	rjmp ISR_0
+;============= VECTORS ============
+
+
+
+
