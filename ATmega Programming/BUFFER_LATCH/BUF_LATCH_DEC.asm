e068a2f7a8c119a95eb1a2e0533e19243971fbd6
Ali Gholami <aligholami7596@gmail.com>
2017-06-05 11:07:59 +0430

    add final project description

---
+++ ATmega Programming/BUFFER_LATCH/BUF_LATCH_DEC.asm
@@ -0,0 +1,9 @@
+;
+; BUF_LATCH_DEC.asm
+;
+; Created: 6/5/2017 11:00:40 AM
+; Author : aligholamee
+;
+; A program to connect 3 interact between the input of the buffers and 7-segments that are connected to the latches
+; On each 7-seg, we will show the number of pressed key according to the buffer that has got the interrupt
+
